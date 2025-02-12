c653c635f5e09b1c1a5d67905d3e33ac17d45114
KunalMitke <kunalsmitke2002@gmail.com>
2025-02-12 12:31:46 +0530

    New

--- array.java
+++ array.java
@@ -4,9 +4,9 @@ public class array {
     public static void main(String args[])
 {
     int a [] = new int[20];
-      for (int i=0; i<a.length; i++)
+      for (int j=0; j<a.length; j++)
       {
-        System.out.println( a[i]);
+        System.out.println( a[j]);
       }
     }   
 }
